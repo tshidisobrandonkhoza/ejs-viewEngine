1a627a8315d9429574766bc0e26331d62193e6d3
brandonkhoza <8brandonkhoza@gmail.com>
2023-11-02 23:01:23 +0200

    Creating ejs files and passing data into views on a skeleton html

--- app.js
+++ app.js
@@ -1,16 +1,53 @@
+
 const express = require('express');
 const app = express();
 
+//register view engine
+
+app.set('view engine', 'ejs');
+// app.set('views', 'otherfolder')
+
+
+//import temp data
+
+// const BLOG = require('./db/blog');
+const BLOG =[];
+
+
 app.get('/', (req, res) => {
-    res.sendFile('./views/index.html', { root: __dirname });
+    res.render('index', {
+        title: 'Ejs Website'
+    });
+    // res.sendFile('./views/index.html', { root: __dirname });
 });
 
 app.get('/about', (req, res) => {
-    res.sendFile('./views/about.html', { root: __dirname });
+    res.render('about', {
+        title: 'Ejs Website'
+    });
+    // res.sendFile('./views/about.html', { root: __dirname });
+});
+
+app.get('/blog', (req, res) => {
+    res.render('blog', {
+        title: 'Ejs Website',
+        blogs: BLOG
+    });
+    // res.sendFile('./views/about.html', { root: __dirname });
+});
+
+app.get('/blog/create', (req, res) => {
+    res.render('create', {
+        title: 'Ejs Website'
+    });
+    // res.sendFile('./views/about.html', { root: __dirname });
 });
 
 app.use((req, res) => {
-    res.status(404).sendFile('./views/404.html', { root: __dirname });
+    res.render('404', {
+        title: 'Ejs Website'
+    });
+    // res.status(404).sendFile('./views/404.html', { root: __dirname });
 });
 
 
---
+++ db/blog.js
@@ -0,0 +1,8 @@
+const BLOG = [
+    { title: 'Red', snippet: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit.' },
+    { title: 'Blue', snippet: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit.' },
+    { title: 'Orange', snippet: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit.' },
+    { title: 'Green', snippet: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit.' },
+];
+
+module.exports = BLOG;
